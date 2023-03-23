38a18f0063e497ec0fcde7295776a66d2b090812
LeoniW101 <leoni101@live.com>
2023-03-23 17:54:54 +0100

    strings assignment upload

---
+++ main.py
@@ -0,0 +1,44 @@
+# Do not modify these lines
+__winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
+__human_name__ = 'strings'
+
+#1.1  Add your code after this line
+#every player that scored
+scoringplayer1 = 'Ruud Gullit'
+scoringplayer2 = 'Marco van Basten'
+
+#1.2 each minute of match that goal was scored
+goal_0 = 32
+goal_1 = 54
+
+# 1.3 Using + create string that reports who scored when
+scorers = scoringplayer1 + " " + str(goal_0)+","" "+ scoringplayer2+ " "+str(goal_1)
+
+#1.4 f string
+report = f"{scoringplayer1} scored in the {goal_0}nd minute {scoringplayer2} scored in the {goal_1}th minute"
+
+# 2.1
+player = 'Ruud Gullit'
+
+#2.2
+first_name = player[:player.find(' ')] #2
+
+#2.3
+last_name_len = len(player[player.find(" "):] [:-1]) # 3
+
+#2.4
+last_name = player[player.find(' '):] # voor name short
+
+name_short = first_name[:-3] + '.' + last_name
+
+#2.5
+chant= len(first_name)*f'{first_name}!' #werkt
+
+#2.6
+good_chant= print(2 != 3) # is 6 een vraag? moet ik iets doen?
+
+
+
+
+
+
